7822440042554f8221acc08264bc5666af63a70b
Meenu Maria <meenu@sputznik.com>
2018-04-09 16:51:01 -0500

    single post page updated with new template

--- single.php
+++ single.php
@@ -2,18 +2,56 @@
 <?php global $post;?>
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-6">
 			<?php if(have_posts()):while ( have_posts() ) : the_post();?> 
-			<h2 class="post-title top-buffer bottom-buffer"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-			<a href="<?php the_permalink(); ?>">
+			<h1 class="post-title"><strong><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></strong></h1>
+			<p class="text-muted">Meta:</p>
+			<div class="media-left">
+				<a href="<?php bloginfo('url');?>" class="rounded">
+					<?php echo get_avatar(32); ?>
+				</a>
+			</div>
+			<div class="media-body text-left">
+				<span class="media-heading"><?php the_author_posts_link(); ?></span>
+				<div class="post-date"><?php echo get_the_date( 'M d' ); ?></div>
+			</div>
+			<div class="top-buffer">
+				<span class="twitter-blue rounded"><i class="fa fa-lg fa-twitter"></i></span>
+				<span class="fb-blue rounded"><i class="fa fa-lg fa-facebook"></i>
+			</div>
+		</div>
+		<div class="col-md-6">
 			<?php if (has_post_thumbnail( $post->ID ) ): ?>
 			  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-				<div class="text-center bottom-buffer"><img class="img-responsive" src="<?php echo $image[0]; ?>" alt="" align="center" ></div>
+				<a href="<?php the_permalink(); ?>" class="card-img overlay bottom-buffer ht-350" style="background-image:url(<?php echo $image[0]?>);" rel="bookmark" title="<?php the_title_attribute(); ?>"></a>
 			<?php endif; ?>
-			</a>
+		</div>
+		<div class="col-md-10 col-md-offset-1 top-buffer">
 			<?php the_content(); ?>
-			<hr class="bottom-buffer">
 			<?php endwhile;endif;?>
+		</div>
+		<div class="col-md-12 no-gutter">
+			<hr class="bottom-buffer">
+			<small class="text-muted col-md-12 bottom-buffer">READ NEXT</small>
+				<?php   global $post;
+				 $current_post = $post; // remember the current post
+
+					  for($i = 1; $i <= 3; $i++):
+						$post = get_previous_post(); // this uses $post->ID
+					  setup_postdata($post);?>
+
+					<div class="col-md-4">
+						<?php if (has_post_thumbnail( $post->ID ) ): ?>
+						  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+							<a href="<?php the_permalink(); ?>" class="card-img overlay bottom-buffer" style="background-image:url(<?php echo $image[0]?>);" rel="bookmark" title="<?php the_title_attribute(); ?>"></a>
+						<?php endif; ?>
+					<h3><strong><a href="<?php the_permalink(); ?>"><?php  the_title(); ?></a></strong></h3>
+					<p class="text-muted"><?php echo  get_the_excerpt(); ?></p>
+				</div>
+					 <?php   endfor;
+						$post = $current_post; 
+
+						?>
 		</div>
 	</div>
 </div>
