e285a3fa71884157d0fdbce1b895da20b09b84e9
Meenu Maria <meenu@sputznik.com>
2018-04-18 12:55:05 -0500

    hdr logo size modified for mobile and font-size and line-ht corrected

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php get_header();?>
-<div class="container-fluid">
+<div class="container-fluid  main-content">
 	<div class="row">
 		<div class="col-md-12">
 			<?php if(have_posts()):while ( have_posts() ) : the_post();?>
