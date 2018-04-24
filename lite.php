b95c4a50ba06b93223f5060506dadbc9ba837b8a
Meenu Maria <meenu@sputznik.com>
2018-04-23 22:54:33 -0500

    lite.php modified

--- lite.php
+++ lite.php
@@ -1,2 +1,2 @@
 <?php /* Template Name: Lite */ ?>	
-<h1 style="text-align:center; margin-top: 2em;">Emergency Helpline: +123456789</h1>
+<h1 style="text-align:center; margin-top: 2em;">Emergency Helpline: +91 11410 91911</h1>
