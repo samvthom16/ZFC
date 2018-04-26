548115bf6b407fd1786bd49e3085f9d1ca845b7c
Samuel Thomas <samvthom16@gmail.com>
2018-04-26 15:12:18 +0530

    Move nav menu options to the parent

--- functions.php
+++ functions.php
@@ -11,23 +11,7 @@
 	<?php
 	});
 	
-	/* CHANGE THE ATTRIBUTES PASSED TO THE NAVIGATION MENU */
-	add_filter('sp_nav_menu_options', function( $sp_nav_menu_options ){
-		
-		global $sp_customize;
-		
-		$header_type = $sp_customize->get_header_type();
-		
-		/* CHANGE ONLY IF HEADER TYPE IS 2 */
-		if( $header_type == 'header2' ){
-		
-			$sp_nav_menu_options['container_class'] = 'collapse navbar-collapse';
-			$sp_nav_menu_options['container_id']    = 'bs-example-navbar-collapse-1';
-			$sp_nav_menu_options['menu_class']      = 'nav navbar-nav navbar-right top-buffer text-uppercase';
-		
-		}	
-		return $sp_nav_menu_options;
-	});
+	
 		
 	add_filter('posts_query_atts', function( $atts ){
 	$atts['title'] = '';
