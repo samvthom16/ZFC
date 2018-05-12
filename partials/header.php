56d6a9940cfcf2d3f337ffbcf299ccaf966b6a21
Meenu Maria <meenu@sputznik.com>
2018-05-12 00:38:33 -0500

    partials/header.php

--- partials/header.php
+++ partials/header.php
@@ -1,16 +1,18 @@
 <nav class="navbar header2">
 	<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle top-buffer" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<?php do_action('sp_logo');?>
-		</div>
-		
-		<?php do_action('sp_nav_menu');?>
-
+			<div class="navbar-header">
+			<div class="navbar-left">
+				<?php do_action('sp_logo');?>
+			<span><?php echo get_bloginfo( 'description', 'display' );?> </span>
+			</div>
+			<div class="nav navbar-nav navbar-right">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			<?php do_action('sp_nav_menu');?>
+	</div>
 	</div>
 	<!-- /.container-fluid --> 
 </nav>
