4406e624a59573ab041d29dbb08686fb4c4d892f
Samuel Thomas <samvthom16@gmail.com>
2018-05-12 21:12:27 +0530

    Update header to fit four logos

--- partials/header.php
+++ partials/header.php
@@ -1,18 +1,42 @@
 <nav class="navbar header2">
 	<div class="container-fluid">
-			<div class="navbar-header">
-			<div class="navbar-left">
-				<?php do_action('sp_logo');?>
-			<span><?php echo get_bloginfo( 'description', 'display' );?> </span>
-			</div>
-			<div class="nav navbar-nav navbar-right">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-			<?php do_action('sp_nav_menu');?>
-	</div>
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle top-buffer" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<?php do_action('sp_logo');?>
+			<div class='clearfix'></div>
+			<div>TAGLINE HERE COME HERE</div>
+		</div>
+		
+		<?php do_action('sp_nav_menu');?>
+
 	</div>
 	<!-- /.container-fluid --> 
 </nav>
+
+<style>
+	
+	.logo{
+		min-height: 80px;
+	}
+	
+	.logo .hidden-xs{
+		max-width: 400px;
+	}
+	
+	@media( max-width: 1200px ){
+		.logo .hidden-xs{
+			max-width: 350px;
+		}
+	}
+	
+	@media( max-width: 1090px ){
+		.logo .hidden-xs{
+			max-width: 320px;
+		}
+	}
+	
+</style>
